2b9c3d1b1e992947a72d75444915bf07946adeda
zoeylinfighting <svzoey@gmail.com>
2017-12-21 23:44:04 +0800

    add category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,4 +3,8 @@ class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 
+  def index
+    @categories = Category.all
+  end
+
 end
