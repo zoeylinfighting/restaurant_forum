2a6f8592dd661b2ddb8b6a1f3059c04e8805b6c6
zoeylinfighting <svzoey@gmail.com>
2017-12-24 16:31:51 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,4 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
 
   before_action :authenticate_admin
   before_action :set_category, :only => [:index, :update, :destroy]
--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,4 @@
-class Admin::RestaurantsController < ApplicationController
+class Admin::RestaurantsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
