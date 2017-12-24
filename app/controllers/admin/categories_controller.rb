2479143c9288fe6acdc7e3ee6599e37301fa8f28
zoeylinfighting <svzoey@gmail.com>
2017-12-24 16:19:08 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,5 @@
 class Admin::CategoriesController < ApplicationController
 
-  before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :set_category, :only => [:index, :update, :destroy]
 
