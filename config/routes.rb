dc3f47440ea4a9efa7310612ae2d2699d2b892c2
zoeylinfighting <svzoey@gmail.com>
2018-01-01 22:30:09 +0800

    Setup routes for user profile page and modify view page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
   end
+
+  resources :users, only:[:show, :edit, :update]
+
   resources :categories, only: :show
   root "restaurants#index" 
 
