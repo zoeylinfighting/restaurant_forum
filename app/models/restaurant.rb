114398fd7e41bb85be1993a2a81eefc86fd8cfee
zoeylinfighting <svzoey@gmail.com>
2018-01-20 08:14:58 +0800

    Revert "add like / unlike button on index page using shared partial"
    This reverts commit 4410caae0315a628569ca79ddc92b1c3f3583eb8.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -17,9 +17,5 @@ class Restaurant < ApplicationRecord
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
-
-  def is_liked?(user)
-    self.liked_users.include?(user)
-  end
   
 end
