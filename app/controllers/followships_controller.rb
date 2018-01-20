41740f43e6cd94471e2c2307c79f1b808abd8f20
zoeylinfighting <svzoey@gmail.com>
2018-01-20 11:26:22 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,16 @@
 class FollowshipsController < ApplicationController
+  def create
+    # 需要設定前端的 link_to, 在發出請求時送進following_id
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed!"
+      redirect_back(fallback_location: root_path)
+    else
+      # 驗證失敗時,Model將錯誤訊息放在errors裡回傳
+      # 使用errors.full_messages 取出完成訊息集合（array）,串接to_sentence將Array 組合成String
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
