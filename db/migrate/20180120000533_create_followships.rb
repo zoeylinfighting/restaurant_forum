bb3472fce2cae888333fa24180a519d1f215fce5
zoeylinfighting <svzoey@gmail.com>
2018-01-22 19:13:06 +0800

    fix lost file

--- db/migrate/20180120000533_create_followships.rb
+++ db/migrate/20180120000533_create_followships.rb
@@ -2,7 +2,7 @@ class CreateFollowships < ActiveRecord::Migration[5.1]
   def change
     create_table :followships do |t|
       t.integer :user_id
-      t.integer :following_id
+      t.integer :followships_id
       t.timestamps
     end
   end
